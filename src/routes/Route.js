4d7361f11272aeb9ab0a88ebb0eb2fe28e93fcf0
felipehenrique159 <felipecod6@hotmail.com>
2021-05-26 01:19:09 -0300

    context api

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,14 +1,15 @@
+import {useContext} from 'react'
 import {Redirect, Route} from 'react-router-dom'
+import { AuthContext } from '../contexts/auth'
 
 export default function RouteWrapper({
     component : Component,
     isPrivate,
     ...rest
 }){
-
-    const loading = false
-    const signed = false
-
+    
+    const {signed,loading} = useContext(AuthContext)
+   
     if(loading){
         return(
             <div></div>
