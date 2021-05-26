741e44e2d13a5f4a006eb32a680d5facde43fb3b
felipehenrique159 <felipecod6@hotmail.com>
2021-05-25 22:18:54 -0300

    configurações firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBkFuG_fMH8qR1NSLD5VEITfyufDrLbAoA",
+    authDomain: "sistema-chamados-2cd22.firebaseapp.com",
+    projectId: "sistema-chamados-2cd22",
+    storageBucket: "sistema-chamados-2cd22.appspot.com",
+    messagingSenderId: "573808048715",
+    appId: "1:573808048715:web:859f525c75c823553dabce",
+    measurementId: "G-JKGYE9YXX4"
+  };
+  
+  if(!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+  }
+
+export default firebase
