74be9c900a43e7d58dc58006831b0a0a533cc088
felipehenrique159 <felipecod6@hotmail.com>
2021-05-28 10:29:02 -0300

    criado e estilizado tabela de chamados dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,15 @@
 import {useState, useContext} from 'react'
 import {AuthContext} from '../../contexts/auth'
 import Header from '../../components/Header'
+import {FiMessageSquare,FiPlus,FiSearch,FiEdit2} from 'react-icons/fi'
+import Title from '../../components/Title'
+import {Link} from 'react-router-dom'
+import './dashboard.css'
+
 export default function Dashboard() {
 
   const {user,logout} = useContext(AuthContext)
-
+  const [chamados,setChamados] = useState([])
 
   function handleLogout(){
     logout()
@@ -13,7 +18,66 @@ export default function Dashboard() {
     return (
       <div>
         <Header/>
-         <button onClick={handleLogout}>LOGOUT</button>
+          <div className="content">
+              <Title nome="Atendimentos">
+                    <FiMessageSquare size={25}/>
+              </Title>
+
+          {chamados.length !== 0 ? (
+             <div className="container dashboard">
+                <span>Nenhum chamado registrado</span>
+                <Link to="/new" className="new">
+                  <FiPlus size={25} color="#FFF"/>
+                    Novo chamado
+                </Link>
+              </div>
+          ) : (
+            <>
+                <Link to="/new" className="new">
+                <FiPlus size={25} color="#FFF"/>
+                  Novo chamado
+                </Link>
+
+                <table>
+                  <thead>
+                    <tr>
+                      <th scope="col">Cliente</th>
+                      <th scope="col">Assunto</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Cadastrado em</th>
+                      <th scope="col">#</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                        <td data-label="Cliente">Sujeito</td>
+                        <td data-label="Suporte">Suporte</td>
+                        <td data-label="Status">
+                          <span className="badge" style={{backgroundColor: '#5cb85c'}}>Em aberto</span>
+                        </td>
+                        <td data-label="Cadastrado">20-06-2021</td>
+                        <td data-label="#">
+                            <button className="action" style={{backgroundColor :'#3583f6'}}>
+                              <FiSearch color="#FFF" size={17}/>
+                            </button>
+                            <button className="action" style={{backgroundColor : '#F6a935'}}>
+                              <FiEdit2 color="#FFF" size={17}/>
+                            </button>
+                        </td>
+                      </tr>
+                  </tbody>
+                </table>
+
+            </>
+          )}
+
+         
+
+          </div>
+
+         
+
+         {/* <button onClick={handleLogout}>LOGOUT</button> */}
       </div>
     )
   }
