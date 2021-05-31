bc221f9cac2da339c856ba6933401ce5cff39c4a
felipehenrique159 <felipecod6@hotmail.com>
2021-05-31 16:59:43 -0300

    mascara de cnpj

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -8,6 +8,7 @@ import firebase from '../../services/firebaseConnection'
 import {toast} from 'react-toastify'
 import { uid } from 'uid'
 import axios from 'axios'
+import CpfCnpj from "@react-br-forms/cpf-cnpj-mask";
 
 export default function Customers(){
 
@@ -17,18 +18,18 @@ export default function Customers(){
     const [numero,setNumero] = useState('')
     const [bairro,setBairro] = useState('')
     const [cidade,setCidade] = useState('')
-
-
+  
     async function handleCadastar(e){
         e.preventDefault()
+        
         if(nomeFantasia !== '' && cnpj !== ''){
 
             await firebase.database().ref('customers').get()
             .then(async(res)=>{
                 let cnpjExistente = false
                 res.forEach((customer)=>{
-                    console.log(customer.val().cnpj);
-                    if(customer.val().cnpj === cnpj){
+                    // console.log(customer.val().cnpj);
+                    if(customer.val().cnpj === cnpj.replace(/[^\d]+/g,'')){
                         cnpjExistente = true
                         toast.error('Cnpj já cadastrado')
                     }
@@ -37,7 +38,7 @@ export default function Customers(){
                 if(!cnpjExistente){
                     await firebase.database().ref('customers/' + uid(16)).set({
                         nomeFantasia: nomeFantasia,
-                        cnpj:cnpj,
+                        cnpj:cnpj.replace(/[^\d]+/g,''),
                         logradouro:logradouro,
                         numero:numero,
                         bairro:bairro,
@@ -73,7 +74,7 @@ export default function Customers(){
         if(cnpj !== ''){
             try {
                 // const response = await axios.get(`http://localhost:3002/buscaDadosReceita/${cnpj}`)       
-                const response = await axios.get(`https://60b50cb898de69e51d790ca6--optimistic-swartz-985c39.netlify.app/.netlify/functions/api/buscaDadosReceita/${cnpj}`)       
+                const response = await axios.get(`https://60b50cb898de69e51d790ca6--optimistic-swartz-985c39.netlify.app/.netlify/functions/api/buscaDadosReceita/${cnpj.replace(/[^\d]+/g,'')}`)       
                 console.log(response.data) 
                 if(response.data.nome){
                     toast.info('Dados encontrados')
@@ -114,7 +115,10 @@ export default function Customers(){
                     <form className="form-profile">
                         <label>Cnpj</label>
                         <div className="cnpj">
-                            <input type="text" value={cnpj} onChange={e => setCnpj(e.target.value)} />
+                            <CpfCnpj
+                            type="text" value={cnpj} onChange={e => {
+                                setCnpj(e.target.value);
+                            }}  />
                             <button onClick={buscarCnpjReceita}>
                             <FiSearch/> <span>Buscar</span>
                         </button>
